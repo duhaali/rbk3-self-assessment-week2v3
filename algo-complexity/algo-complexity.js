e65e34f9ced4513f9af18df8e3e5bc34438b4f08
unknown <dohaali425@gmail.com>
2017-09-04 09:08:06 +0300

    hehe

--- algo-complexity/algo-complexity.js
+++ algo-complexity/algo-complexity.js
@@ -8,47 +8,47 @@
 // Problem 1: Retrieval of a value from a hash table using the following function with regard to the size of the hash table. knowing that the key never get very large
 
 /*
- Complexity:
+ Complexity: the time complexity is O(n) , because all statment constant except the for loop will O(1)+ O(n)+ O(1)= O(n) 
  */
 
 var retrieve = function(key) {
-  var hash = 0;
+  var hash = 0;  //O(1)
   for (var i = 0; i < key.length; i++) {
     hash = (hash + Math.pow(i, hash)) % array.length;
-  }
-  return array[hash];
+  }  // O(n)
+  return array[hash]; // O(1)
 };
 
 
 // Problem 2: sortedArrayContainsItem with regard to the length of the passed-in array
 
 /*
- Complexity:
+ Complexity:the time complexity is O(n) , because O(1)+ O(1)+ O(1)+ O(n)= O(n) 
  */
 
 
 var sortedArrayContainsItem = function(array, item) {
-  var center = Math.floor(array.length / 2);
+  var center = Math.floor(array.length / 2); //O(1)
   if (array[center] === item) {
     return true;
-  }
-  var halfOfArray = item < array[center] ? array.slice(0, center) : array.slice(center);
-  return sortedArrayContainsItem(halfOfArray, item);
+  } //O(1)
+  var halfOfArray = item < array[center] ? array.slice(0, center) : array.slice(center); //O(1)
+  return sortedArrayContainsItem(halfOfArray, item); //O(n) for recarcen
 };
 
 
 // Problem 3: hasDuplicates with regard to the length of the passed in array
 
 /*
- Complexity:
+ Complexity: the time complexity is O(n) , because we have one loop and it is not insted loop will O(1)+ O(1)+ O(n)= O(n) 
  */
 
 var hasDuplicates = function(array) {
   for (var i = 0; i < array.length; i++) {
-    var item = array[i];
+    var item = array[i]; //O(1)
     if (array.slice(i + 1).indexOf(item) !== -1) {
       return true;
-    }
-  }
+    } // O(1)
+  } //O(n)
   return false;
 };
--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -4,3 +4,9 @@
 /*
 
 */
+
+constant complexity withe equal O(1) it is for simple equajen or seclarate variable EX: var x, x=6+8
+liniar   complexity withe equal O(n) its for for lob statment EX: for (var i = 0; i < Things.length; i++) {}
+quadratic complexity withe equal O log(n) for instans for loop 
+the fourth one when we have a multiple thing like loops and recargen 
+
